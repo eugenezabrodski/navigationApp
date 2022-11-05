96359e454282ae25ed73d594fbd0f273ef2bb6fb
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2022-11-05 17:46:37 +0300

    0.1

--- navigationApp/navigationApp/SceneDelegate.swift
+++ navigationApp/navigationApp/SceneDelegate.swift
@@ -10,12 +10,37 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    var navController = UINavigationController()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+       
+        var viewController = ViewController()
+        self.navController = UINavigationController(rootViewController: viewController)
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = self.navController
+        
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            
+            let viewController = ViewController()
+            let navController = UINavigationController(rootViewController: viewController)
+
+            window.rootViewController = navController
+
+            self.window = window
+            window.backgroundColor = UIColor.white
+            window.makeKeyAndVisible()
+        }
+        
+        
+        
+        
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
